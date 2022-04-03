7276f694ee58d96caee2ac54b5e006dc93656aaf
Sazzad12340 <sazzadrn17@gmail.com>
2022-04-03 17:10:12 +0600

    portfolio

--- assets/app.js
+++ assets/app.js
@@ -1,21 +1,18 @@
-window.onscroll = function () {
-  myFunction();
-};
+window.onscroll = scrollShowNav;
+let navEle = document.getElementsByTagName("nav")[0];
+let stickyTop = navEle.offsetTop;
 
-const navbar = document.querySelector(".navbar");
-
-let sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
+function scrollShowNav() {
+  if (window.pageYOffset >= stickyTop) {
+    navEle.classList.add("fixed");
   } else {
-    navbar.classList.remove("sticky");
+    console.log("WOW");
+    navEle.classList.remove("fixed");
   }
 }
 
 const hambergur = document.getElementById("hamburger");
-const navUl = document.querySelector(".nav-list");
+const navUl = document.querySelector(".navlist");
 
 hambergur.addEventListener("click", () => {
   navUl.classList.toggle("show");
